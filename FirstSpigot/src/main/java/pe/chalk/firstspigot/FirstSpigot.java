59cadf97b20f54d62a21895399f7dc71928bb240
ChalkPE <amato0617@gmail.com>
2015-04-12 12:32:08 +0900

    Removed some classes

--- FirstSpigot/src/main/java/pe/chalk/firstspigot/FirstSpigot.java
+++ FirstSpigot/src/main/java/pe/chalk/firstspigot/FirstSpigot.java
@@ -1,34 +1,47 @@
 package pe.chalk.firstspigot;
 
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * @author ChalkPE <amato0617@gmail.com>
  * @since 2015-04-12
  */
-public class FirstSpigot extends JavaPlugin {
-    private static FirstSpigotCommand fsc;
-
+public class FirstSpigot extends JavaPlugin implements Listener, CommandExecutor {
     @Override
     public void onEnable(){
         super.onEnable();
-        initCommand();
-        initEvent();
+
+        this.getCommand("test").setExecutor(this);
+        this.getCommand("test2").setExecutor(this);
+        this.getServer().getPluginManager().registerEvents(this, this);
+
         this.getLogger().info("Hello, Spigot!");
     }
+
     @Override
     public void onDisable() {
         super.onDisable();
-        this.getLogger().info("Bye, Spigot!");
+
+        this.getLogger().info("Goodbye, Spigot!");
     }
-    public void initCommand() {
-        fsc = new FirstSpigotCommand(this);
-        getCommand("test").setExecutor(fsc);
-        getCommand("test2").setExecutor(fsc);
-    }
-    public void initEvent() {
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(new FirstSpigotEvent(this), this);
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)){
+            this.getServer().getLogger().warning("Please run this command in-game!");
+            return false;
+        }
+
+        if(command.getName().equalsIgnoreCase("test")) {
+            sender.sendMessage("Hello, commander!");
+        } else if(command.getName().equalsIgnoreCase("test2")) {
+            this.getServer().broadcastMessage("Hello, world!");
+        }
+        return true;
     }
 }
