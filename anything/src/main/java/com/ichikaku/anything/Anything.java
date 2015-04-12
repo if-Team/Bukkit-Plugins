22211af80bad57112f7af1f13d62075e3bd48b18
일가크 <woni8708@naver.com>
2015-04-12 14:56:31 +0900

    make plugin "anything"

---
+++ anything/src/main/java/com/ichikaku/anything/Anything.java
@@ -0,0 +1,58 @@
+package com.ichikaku.anything;
+
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.material.Tree;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.*;
+import org.bukkit.util.Vector;
+
+public class Anything extends JavaPlugin implements Listener, CommandExecutor {
+    @Override
+    public void onEnable(){
+        super.onEnable();
+
+        this.getCommand("test").setExecutor(this);
+        this.getCommand("test2").setExecutor(this);
+        this.getServer().getPluginManager().registerEvents(this, this);
+
+        this.getLogger().info("Hello");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+        this.getLogger().info("Goodbye");
+    }
+
+    public void onBreakEvent(BlockBreakEvent e) {
+        Player player = e.getPlayer();
+        Block b = e.getBlock();
+        Location v = b.getLocation();
+        World w = player.getWorld();
+        if(b.getType() == Material.WOOD) w.strikeLightning(v);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)){
+            this.getServer().getLogger().warning("Please run this command in-game!");
+            return false;
+        }
+
+        if(command.getName().equalsIgnoreCase("test")) {
+            sender.sendMessage(ChatColor.AQUA + "Hello, commander!");
+        } else if(command.getName().equalsIgnoreCase("test2")) {
+            this.getServer().broadcastMessage(ChatColor.LIGHT_PURPLE + "Hello, world!");
+        }
+        return true;
+    }
+}
